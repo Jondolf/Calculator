fc9b55d6168a0f279eee3f5e71cc5d552e480ae2
Jondolf <joona.m.aalto@outlook.com>
2020-06-07 20:38:26 +0300

    Clean up basic calculator code & add functionality Now calculations like 3π, 4e, ππ, πe etc. work ( multiplication syntax)

--- src/app/calculator/calculators/basic-calculator/basic-calculator.service.ts
+++ src/app/calculator/calculators/basic-calculator/basic-calculator.service.ts
@@ -4,6 +4,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class BasicCalculatorService {
+  countCalculation(calculation: string): number {
+    // With working code math operators (*, / etc.) instead of x and ÷ etc.
+    const realCalculation = calculation.replace(/x/g, '*').replace(/÷/g, '/').replace(/mod/g, 'm')
+      .replace(/sin/g, 's').replace(/cos/g, 'c').replace(/tan/g, 't').replace(/([0-9πe])π/g, '$1*π').replace(/([0-9πe])e/g, '$1*e');
+    const result = this.countPlus(realCalculation);
+    return result;
+  }
+
   split(calculation: string, operator: string): string[] {
     const result = [];
     let braces = 0;
@@ -25,8 +33,49 @@ export class BasicCalculatorService {
     return result;
   }
 
+  // +, -, *, /, **, %
+  countPlus(calculation: string): number {
+    const numberStrings: string[] = this.split(calculation, '+');
+    const numbers: number[] = numberStrings.map((numberString: string) => this.countMinus(numberString));
+    const intitialValue = 0.0;
+    const result = numbers.reduce((acc: number, num: number) => acc + num, intitialValue);
+    return result;
+  }
+  // -, *, /, **, %
+  countMinus(calculation: string): number {
+    const numberStrings: string[] = this.split(calculation, '-');
+    const numbers: number[] = numberStrings.map((numberString: string) => this.countMultiply(numberString));
+    const intitialValue = numbers[0];
+    const result = numbers.slice(1).reduce((acc: number, num: number) => acc - num, intitialValue);
+    return result;
+  }
+  // *, /, **, %
+  countMultiply(calculation: string): number {
+    const numberStrings: string[] = this.split(calculation, '*');
+    const numbers = numberStrings.map((numberString: string) => this.countDivide(numberString));
+    const intitialValue = 1.0;
+    const result = numbers.reduce((acc: number, num: number) => acc * num, intitialValue);
+    return result;
+  }
+  // /, **, %
+  countDivide(calculation: string): number {
+    const numberStrings: string[] = this.split(calculation, '/');
+    const numbers = numberStrings.map((numberString: string) => this.countPow(numberString));
+    const intitialValue = numbers[0];
+    const result = numbers.slice(1).reduce((acc: number, num: number) => acc / +num, intitialValue);
+    return result;
+  }
+  // **, %
+  countPow(calculation: string): number {
+    const numberStrings: string[] = this.split(calculation, '^');
+    const numbers = numberStrings.map((numberString: string) => this.countModulo(numberString));
+    const intitialValue = numbers[0];
+    const result = numbers.slice(1).reduce((acc: number, num: number | string) => acc ** +num, intitialValue);
+    return result;
+  }
+  // %
   countModulo(calculation: string): number {
-    const numberStrings: string[] = this.split(calculation, 'm'); // m is mod here because % is it's own command
+    const numberStrings: string[] = this.split(calculation, 'm'); // m means modulo here because % is already it's own command
     const numbers = numberStrings.map((numberString: string) => {
       if (numberString[0] === '(') {
         const calc = numberString.substr(1, numberString.length - 2);
@@ -64,48 +113,8 @@ export class BasicCalculatorService {
     return result;
   }
 
-  countPow(calculation: string): number {
-    const numberStrings: string[] = this.split(calculation, '^');
-    const numbers = numberStrings.map((numberString: string) => this.countModulo(numberString));
-    const intitialValue = numbers[0];
-    const result = numbers.slice(1).reduce((acc: number, num: number | string) => acc ** +num, intitialValue);
-    return result;
-  }
-
-  // /
-  countDivide(calculation: string): number {
-    const numberStrings: string[] = this.split(calculation, '/');
-    const numbers = numberStrings.map((numberString: string) => this.countPow(numberString));
-    const intitialValue = numbers[0];
-    const result = numbers.slice(1).reduce((acc: number, num: number) => acc / +num, intitialValue);
-    return result;
-  }
-  // *, /
-  countMultiply(calculation: string): number {
-    const numberStrings: string[] = this.split(calculation, '*');
-    const numbers = numberStrings.map((numberString: string) => this.countDivide(numberString));
-    const intitialValue = 1.0;
-    const result = numbers.reduce((acc: number, num: number) => acc * num, intitialValue);
-    return result;
-  }
-  // -, *, /
-  countMinus(calculation: string): number {
-    const numberStrings: string[] = this.split(calculation, '-');
-    const numbers: number[] = numberStrings.map((numberString: string) => this.countMultiply(numberString));
-    const intitialValue = numbers[0];
-    const result = numbers.slice(1).reduce((acc: number, num: number) => acc - num, intitialValue);
-    return result;
-  }
-  // +, -, *, /
-  countPlus(calculation: string): number {
-    const numberStrings: string[] = this.split(calculation, '+');
-    const numbers: number[] = numberStrings.map((numberString: string) => this.countMinus(numberString));
-    const intitialValue = 0.0;
-    const result = numbers.reduce((acc: number, num: number) => acc + num, intitialValue);
-    return result;
-  }
-
   countFactorial(num: number): number {
+    // Avoid ridiculously large numbers for better performance
     if (num > 200) {
       return Infinity;
     }
@@ -113,15 +122,6 @@ export class BasicCalculatorService {
     for (let i = 2; i <= num; i++) {
       result *= i;
     }
-    return result;
-  }
-
-  countCalculation(calculation: string): number {
-    // With proper code math operators (*, /) instead of x and ÷
-    const realCalculation = calculation.replace(/x/g, '*').replace(/÷/g, '/').replace(/mod/g, 'm')
-      .replace(/sin/g, 's').replace(/cos/g, 'c').replace(/tan/g, 't');
-    const result = this.countPlus(realCalculation);
-    // console.log('Result', realCalculation, result);
     return result;
   }
 }
