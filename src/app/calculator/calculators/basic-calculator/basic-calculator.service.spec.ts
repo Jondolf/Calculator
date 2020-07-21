78d9f650cb28f54832b044db4d11590d9518ee8c
Jondolf <joona.m.aalto@outlook.com>
2020-07-21 14:53:52 +0300

    Remove unused import

--- src/app/calculator/calculators/basic-calculator/basic-calculator.service.spec.ts
+++ src/app/calculator/calculators/basic-calculator/basic-calculator.service.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { BasicCalculatorService } from './basic-calculator.service';
 
 describe('Service: BasicCalculator', () => {
