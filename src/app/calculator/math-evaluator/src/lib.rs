f7dfbc3367893a38084c44bd6b4101ac80bd45c4
Jondolf <57632562+Jondolf@users.noreply.github.com>
2021-07-03 16:17:05 +0300

    Add deg variants of more trigonometric functions
    Added degree support for:
    asin, acos, atan,
    sinh, cosh, tanh,
    asinh, acosh, atanh

--- src/app/calculator/math-evaluator/src/lib.rs
+++ src/app/calculator/math-evaluator/src/lib.rs
@@ -17,6 +17,15 @@ fn eval(expr: &str, var_name: &str, var_val: f64) -> f64 {
         .func("sind", |x: f64| x.to_radians().sin())
         .func("cosd", |x: f64| x.to_radians().cos())
         .func("tand", |x: f64| x.to_radians().tan())
+        .func("asind", |x: f64| x.to_radians().asin())
+        .func("acosd", |x: f64| x.to_radians().acos())
+        .func("atand", |x: f64| x.to_radians().atan())
+        .func("sindh", |x: f64| x.to_radians().sinh())
+        .func("cosdh", |x: f64| x.to_radians().cosh())
+        .func("tandh", |x: f64| x.to_radians().tanh())
+        .func("asindh", |x: f64| x.to_radians().asinh())
+        .func("acosdh", |x: f64| x.to_radians().acosh())
+        .func("atandh", |x: f64| x.to_radians().atanh())
         .func("lb", |x: f64| x.log2());
 
     match meval::eval_str_with_context(expr, ctx) {
